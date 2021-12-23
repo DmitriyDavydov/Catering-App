dc945a130caea1b5dd2d355b97a21f067745d183
Dmytro Davydov <dmytro.davydov@sigma.software>
2021-12-23 15:47:29 +0200

    Initial Commit

---
+++ Catering App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Catering App
+//
+//  Created by ddavydov on 23.12.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
