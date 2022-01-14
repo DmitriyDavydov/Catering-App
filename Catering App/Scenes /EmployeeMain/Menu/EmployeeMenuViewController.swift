ce9e942dc457b39d2f70833f68254594bc5049be
Dmytro Davydov <dmytro.davydov@sigma.software>
2022-01-14 15:16:32 +0200

    Added alert and invocation mech in EmployeeMenuVC

--- Catering App/Scenes /EmployeeMain/Menu/EmployeeMenuViewController.swift
+++ Catering App/Scenes /EmployeeMain/Menu/EmployeeMenuViewController.swift
@@ -13,6 +13,22 @@ class EmployeeMenuViewController: UIViewController {
     let firebaseFirestoreQueryManager = FirebaseFirestoreQueryManager()
     let firebaseStorageManager = FirebaseStorageManager()
     
+    //alertview
+    let alertBackgroundView = UIView()
+    let alertMainContainer = UIView()
+    let alertTitle = UILabel()
+    let alertSaveButton = UIButton()
+    let alertCloseButton = UIButton()
+
+    let nameTextField = UITextField()
+    let descriptionTextField = UITextField()
+    let portionTextField = UITextField()
+    let categoryTextField = UITextField()
+    let chevronTextField = UITextField()
+    let priceTextField = UITextField()
+    let textFieldsStack = UIStackView()
+    
+    //screen
     let backgroundView = UIView()
     let tableView = UITableView()
     let searchBar = UISearchBar()
@@ -41,7 +57,7 @@ class EmployeeMenuViewController: UIViewController {
         addButton.setTitle("ADD", for: .normal)
         addButton.setTitleColor(.black, for: .normal)
         addButton.titleLabel?.font = UIFont.systemFont(ofSize: 20, weight: .bold)
-        addButton.addTarget(self, action: #selector(invokeAlert), for: .touchUpInside)
+        addButton.addTarget(self, action: #selector(invokeAddingAlert), for: .touchUpInside)
         
         searchBar.searchBarStyle = .minimal
     }
@@ -83,79 +99,121 @@ class EmployeeMenuViewController: UIViewController {
     
     
     // MARK: invokeAlert
-    @objc func invokeAlert() {
-        
-        print("invokeAlert tapped")
-        /*
-        let alertBackgroundView = UIView()
-        let alertMainContainer = UIView()
-        let alertTitle = UILabel()
-        let alertAddButton = UIButton()
-
-        let name = UITextField()
-        let description = UITextField()
-        let portion = UITextField()
-        let category = UITextField()
-        let chevron = UITextField()
-        let price = UITextField()
-        let photo = UIImagePickerController()
-        let textFieldsStack = UIStackView()
-        
+    @objc func invokeAddingAlert() {
         backgroundView.addSubview(alertBackgroundView)
         backgroundView.addSubview(alertMainContainer)
         backgroundView.addSubview(alertTitle)
-        backgroundView.addSubview(alertAddButton)
+        backgroundView.addSubview(alertSaveButton)
+        backgroundView.addSubview(alertCloseButton)
         backgroundView.addSubview(textFieldsStack)
         
-        textFieldsStack.addSubview(name)
-        textFieldsStack.addSubview(description)
-        textFieldsStack.addSubview(portion)
-        textFieldsStack.addSubview(category)
-        textFieldsStack.addSubview(chevron)
-        textFieldsStack.addSubview(price)
-        
-        alertBackgroundView.backgroundColor = .lightGray
-        alertBackgroundView.alpha = 0.5
-        alertMainContainer.backgroundColor = .white
-        alertTitle.text = "ADD NEW ITEM"
-        alertAddButton.setTitle("ADD", for: .normal)
-        alertAddButton.setTitleColor(.black, for: .normal)
-        alertAddButton.titleLabel?.font = UIFont.systemFont(ofSize: 20, weight: .bold)
-        name.text = "name"
-        name.textColor = .black
-        name.clearsOnBeginEditing = true
-
-        textFieldsStack.alignment = .leading
-        textFieldsStack.distribution = .equalSpacing
-        textFieldsStack.axis = .vertical
-        textFieldsStack.spacing = 5
+        textFieldsStack.addArrangedSubview(nameTextField)
+        textFieldsStack.addArrangedSubview(descriptionTextField)
+        textFieldsStack.addArrangedSubview(portionTextField)
+        textFieldsStack.addArrangedSubview(categoryTextField)
+        textFieldsStack.addArrangedSubview(chevronTextField)
+        textFieldsStack.addArrangedSubview(priceTextField)
         
         alertBackgroundView.frame = backgroundView.bounds
+        alertBackgroundView.backgroundColor = .black
+        alertBackgroundView.alpha = 0.8
+        
+        alertMainContainer.backgroundColor = .white
+        alertMainContainer.layer.cornerRadius = 10
+        
+        alertTitle.text = "ADD NEW ITEM"
+        alertTitle.font = UIFont.systemFont(ofSize: 20, weight: .bold)
+        
+        alertSaveButton.setTitle("SAVE", for: .normal)
+        alertSaveButton.setTitleColor(.white, for: .normal)
+        alertSaveButton.titleLabel?.font = UIFont.systemFont(ofSize: 20, weight: .bold)
+        alertSaveButton.backgroundColor = .black
+        alertSaveButton.addTarget(self, action: #selector(saveMenuItem), for: .touchUpInside)
+        
+        alertCloseButton.setTitle("x", for: .normal)
+        alertCloseButton.setTitleColor(.black, for: .normal)
+        alertCloseButton.titleLabel?.font = UIFont.systemFont(ofSize: 25, weight: .bold)
+        alertCloseButton.addTarget(self, action: #selector(dismissAddingAlert), for: .touchUpInside)
+        
+        create(textField: nameTextField, with: "Name")
+        create(textField: descriptionTextField, with: "Description")
+        create(textField: portionTextField, with: "Portion")
+        create(textField: categoryTextField, with: "Category")
+        create(textField: chevronTextField, with: "Chevron")
+        create(textField: priceTextField, with: "Price")
 
+        textFieldsStack.alignment = .fill
+        textFieldsStack.distribution = .equalSpacing
+        textFieldsStack.axis = .vertical
+        textFieldsStack.spacing = 10
+        
         alertMainContainer.translatesAutoresizingMaskIntoConstraints = false
-        alertMainContainer.widthAnchor.constraint(equalToConstant: 300).isActive = true
-        alertMainContainer.heightAnchor.constraint(greaterThanOrEqualToConstant: 700).isActive = true
-        alertMainContainer.centerXAnchor.constraint(equalTo: backgroundView.centerXAnchor).isActive = true
-        alertMainContainer.centerYAnchor.constraint(equalTo: backgroundView.centerYAnchor).isActive = true
-
         alertTitle.translatesAutoresizingMaskIntoConstraints = false
-        alertTitle.centerXAnchor.constraint(equalTo: backgroundView.centerXAnchor).isActive = true
-        alertTitle.topAnchor.constraint(equalTo: alertMainContainer.topAnchor, constant: 10).isActive = true
-        alertTitle.heightAnchor.constraint(greaterThanOrEqualToConstant: 50).isActive = true
-        alertTitle.widthAnchor.constraint(lessThanOrEqualToConstant: 300).isActive = true
-        
-        alertAddButton.translatesAutoresizingMaskIntoConstraints = false
-        alertAddButton.centerXAnchor.constraint(equalTo: backgroundView.centerXAnchor).isActive = true
-        alertAddButton.bottomAnchor.constraint(equalTo: alertMainContainer.bottomAnchor, constant: -10).isActive = true
-        alertAddButton.heightAnchor.constraint(greaterThanOrEqualToConstant: 50).isActive = true
-        alertAddButton.widthAnchor.constraint(lessThanOrEqualToConstant: 70).isActive = true
-        
+        alertSaveButton.translatesAutoresizingMaskIntoConstraints = false
+        alertCloseButton.translatesAutoresizingMaskIntoConstraints = false
         textFieldsStack.translatesAutoresizingMaskIntoConstraints = false
-        textFieldsStack.centerXAnchor.constraint(equalTo: backgroundView.centerXAnchor).isActive = true
-        textFieldsStack.topAnchor.constraint(equalTo: alertTitle.bottomAnchor, constant: 10).isActive = true
-        textFieldsStack.bottomAnchor.constraint(equalTo: alertAddButton.topAnchor, constant: -10).isActive = true
-        */
+        
+        NSLayoutConstraint.activate([
+            alertMainContainer.widthAnchor.constraint(equalToConstant: backgroundView.frame.width * 0.9),
+            alertMainContainer.heightAnchor.constraint(greaterThanOrEqualToConstant: 300),
+            alertMainContainer.centerXAnchor.constraint(equalTo: backgroundView.centerXAnchor),
+            alertMainContainer.centerYAnchor.constraint(equalTo: backgroundView.centerYAnchor),
+            
+            alertTitle.centerXAnchor.constraint(equalTo: backgroundView.centerXAnchor),
+            alertTitle.topAnchor.constraint(equalTo: alertMainContainer.topAnchor, constant: 10),
+            alertTitle.heightAnchor.constraint(greaterThanOrEqualToConstant: 50),
+            alertTitle.widthAnchor.constraint(greaterThanOrEqualToConstant: 100),
+            
+            alertSaveButton.centerXAnchor.constraint(equalTo: backgroundView.centerXAnchor),
+            alertSaveButton.bottomAnchor.constraint(equalTo: alertMainContainer.bottomAnchor, constant: -10),
+            alertSaveButton.heightAnchor.constraint(greaterThanOrEqualToConstant: 30),
+            alertSaveButton.widthAnchor.constraint(greaterThanOrEqualToConstant: 90),
+            
+            alertCloseButton.topAnchor.constraint(equalTo: alertMainContainer.topAnchor, constant: 10),
+            alertCloseButton.trailingAnchor.constraint(equalTo: alertMainContainer.trailingAnchor, constant: -10),
+            alertCloseButton.widthAnchor.constraint(equalToConstant: 30),
+            alertCloseButton.heightAnchor.constraint(equalToConstant: 30),
+            
+            textFieldsStack.centerXAnchor.constraint(equalTo: backgroundView.centerXAnchor),
+            textFieldsStack.topAnchor.constraint(equalTo: alertTitle.bottomAnchor, constant: 20),
+            textFieldsStack.bottomAnchor.constraint(equalTo: alertSaveButton.topAnchor, constant: -30),
+            textFieldsStack.leadingAnchor.constraint(equalTo: alertMainContainer.leadingAnchor, constant: 15),
+            textFieldsStack.trailingAnchor.constraint(equalTo: alertMainContainer.trailingAnchor, constant: -15)
+        ])
+        
+        func create(textField: UITextField, with initialText: String ) {
+            textField.text = initialText
+            textField.backgroundColor = .lightGray
+            textField.textColor = .black
+            textField.clearsOnBeginEditing = true
+            textField.font = UIFont.systemFont(ofSize: 20, weight: .regular)
+        }
+
     }
+    
+    // MARK: dismissAlert
+    @objc func dismissAddingAlert() {
+        alertBackgroundView.removeFromSuperview()
+        alertMainContainer.removeFromSuperview()
+        alertTitle.removeFromSuperview()
+        alertCloseButton.removeFromSuperview()
+        
+        nameTextField.removeFromSuperview()
+        descriptionTextField.removeFromSuperview()
+        portionTextField.removeFromSuperview()
+        categoryTextField.removeFromSuperview()
+        chevronTextField.removeFromSuperview()
+        priceTextField.removeFromSuperview()
+        textFieldsStack.removeFromSuperview()
+    }
+    
+    // MARK: saveMenuItem
+    @objc func saveMenuItem() {
+        print("saveMenuItem is trigerred")
+        
+        dismissAddingAlert()
+    }
+     
 
 }
 
